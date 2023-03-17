bf176863d987a806fe6d393d5318f59d7a7c0892
conglt <thanhcong1710@gmail.com>
2023-03-17 17:48:37 +0700

    udpate

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/{any}', function () {
     return view('coreui.homepage');
 })->where('any', '.*');
 
 //Auth::routes();
-//Route::get('/home', 'HomeController@index')->name('home');
+
 
 /*
 Route::post('register', 'AuthController@register');
